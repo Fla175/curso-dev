07d3b7966c393728a909f2aa6a4102a770c8bb79
Fla175 <flaviofpomin@gmail.com>
2025-08-25 19:40:26 +0000

    Add - (Slim-Framework - guiaCurl.html e alterações em routes.php)

--- slim-framework/app/routes.php
+++ slim-framework/app/routes.php
@@ -2,25 +2,20 @@
 
 declare(strict_types=1);
 
-// ... (seus 'use' statements aqui)
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 
 return function (App $app) {
 
-    // Rota GET para listar postagens (já existe)
     $app->get('/postagens', function(Request $request, Response $response) {
         $response->getBody()->write("Lista de Postagens");
         return $response;
     });
 
-    // Rota POST para criar uma nova postagem (A ROTA QUE ESTAVA FALTANDO)
     $app->post('/postagens', function(Request $request, Response $response) {
-        // Obtenha os dados do corpo da requisição (JSON)
         $dados = $request->getParsedBody();
-        
-        // Verifique se os dados existem e não estão vazios
+
         if (!empty($dados)) {
             $response->getBody()->write("Dados recebidos (JSON): " . json_encode($dados, JSON_PRETTY_PRINT));
         } else {
@@ -29,4 +24,41 @@ return function (App $app) {
         
         return $response;
     });
+
+    // método curl para a rota /usuarios/adiciona: curl -X POST -H "Content-Type: application/json" -d '{"id": 1, "nome": "Flavio", "email": "flavio@exemplo.com"}' http://10.20.31.152:8080/proxy/5040/usuarios/adiciona
+
+    $app->post('/usuarios/adiciona', function(Request $request, Response $response) {
+
+        $post = $request->getParsedBody();
+        $id = $post['id'];
+        $nome = $post['nome'];
+        $email = $post['email'];
+
+        $response->getBody()->write("Sucesso ao adicionar" . "                                                                             ");
+        return $response;
+    });
+
+    // método curl para a rota /usuarios/atualiza: curl -X PUT -H "Content-Type: application/json" -d '{"id": 1, "nome": "Flavio Atualizado", "email": "flavio.novo@exemplo.com"}' http://10.20.31.152:8080/proxy/5040/usuarios/atualiza/1
+
+    $app->put('/usuarios/atualiza/{id}', function(Request $request, Response $response) {
+
+        $post = $request->getParsedBody();
+        $id = $post['id'];
+        $nome = $post['nome'];
+        $email = $post['email'];
+
+        $response->getBody()->write("Sucesso ao atualizar: " . $id . "                                                                          ");
+        return $response;
+    });
+
+    // método curl para a rota /usuarios/remove: curl -X DELETE http://10.20.31.152:8080/proxy/5040/usuarios/remove/1
+
+    $app->delete('/usuarios/remove/{id}', function(Request $request, Response $response) {
+        $id = $request->getAttribute('id');
+
+        $response->getBody()->write("Sucesso ao remover: " . $id . "                                                                            ");
+        return $response;
+    });
+
 };
+
